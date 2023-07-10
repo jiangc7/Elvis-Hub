9639d63efffdd887e26fcdf92be3428b75cfbeb4
jiangc7 <118397495+jiangc7@users.noreply.github.com>
2023-07-10 17:17:35 +1200

    create a customer hook

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:'https://api.rawg.io/api',
     params:{
-        key:'c7b18323a47d40c394ea5b019646b1f5'
+        key:'3532e930d85148ec868c4e97571ba32a'
     },
 })
