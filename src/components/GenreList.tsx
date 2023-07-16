ccd073ffae18d6b9e402d4da2dafc10e18e705e3
jiangc7 <118397495+jiangc7@users.noreply.github.com>
2023-07-16 22:11:59 +1200

    fitler the genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 import useGenres, { Genre } from '../hooks/useGenres'
 import useData from '../hooks/useData';
-import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
+import { Button, HStack, Image, List, ListItem, Spinner, Text, withDefaultVariant } from '@chakra-ui/react';
 import getCroppedImageUrl from '../services/image-url';
 
-const GenreList = () => {
+interface Props {
+  onSelectedGenre: (genre: Genre) =>void;
+}
+
+const GenreList = ({onSelectedGenre}: Props) => {
     const {data, isLoading,error } = useGenres();
 
     if(error) return null
@@ -17,7 +21,7 @@ const GenreList = () => {
             <ListItem key={genres.id} paddingY='5px'>
                 <HStack>
                     <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genres.image_background) } />
-                    <Text fontSize={'lg'}>{genres.name}</Text>
+                    <Button onClick={() => onSelectedGenre(genres)} fontSize='lg' variant='link'>{genres.name}</Button>
                 </HStack>
             </ListItem>
             ))}
