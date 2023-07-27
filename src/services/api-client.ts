d8b46829ebff57e3173a658b585d7bcd65d06d5d
jiangc7 <118397495+jiangc7@users.noreply.github.com>
2023-07-27 21:11:18 +1200

    change to domain

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'http://3.104.98.246/api/',
+    baseURL:'http://www.elvisjiang.com/api/',
     params:{
        // key:'3532e930d85148ec868c4e97571ba32a'
     },
